697e7296bc132234253ddc9f37269112a7bce852
Cex0 <981870450@qq.com>
2019-12-26 01:24:08 +0800

    project add CommonResult

---
+++ src/main/java/com/cex0/mobiai/core/CommonResultControllerAdvice.java
@@ -0,0 +1,67 @@
+package com.cex0.mobiai.core;
+
+import com.cex0.mobiai.model.BaseResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @Auther: wodenvyoujiaoshaxiong
+ * @Date: 2019/12/26 00:15
+ * @Description:
+ */
+@ControllerAdvice("com.cex0.mobiai.controller")
+public class CommonResultControllerAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> converterType) {
+        return AbstractJackson2HttpMessageConverter.class.isAssignableFrom(converterType);
+    }
+
+    @Override
+    public Object beforeBodyWrite(@Nullable Object body, MethodParameter returnType,
+                                  MediaType contentType, Class<? extends HttpMessageConverter<?>> converterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        MappingJacksonValue container = getOrCreateContainer(body);
+        //这个响应体不会为空
+        beforeBodyWriteInternal(container, contentType, returnType, request, response);
+        return container;
+    }
+
+    private void beforeBodyWriteInternal(MappingJacksonValue bodyContainer,
+                                         MediaType contentType,
+                                         MethodParameter returnType,
+                                         ServerHttpRequest request,
+                                         ServerHttpResponse response) {
+        // 获取返回对象
+        Object returnBody = bodyContainer.getValue();
+
+        if (returnBody instanceof BaseResponse) {
+            // 如果是BaseResponse类型
+            BaseResponse<?> baseResponse = (BaseResponse) returnBody;
+            response.setStatusCode(HttpStatus.resolve(baseResponse.getStatus()));
+            return;
+        }
+
+        // 包裹返回实体
+        BaseResponse<?> baseResponse = BaseResponse.ok(returnBody);
+        bodyContainer.setValue(baseResponse);
+        response.setStatusCode(HttpStatus.valueOf(baseResponse.getStatus()));
+    }
+
+
+    /**
+     * 将响应体包在{@link MappingJacksonValue}值容器中，如果已经包装好了就转换他
+     */
+    private MappingJacksonValue getOrCreateContainer(Object body) {
+        return (body instanceof MappingJacksonValue ? (MappingJacksonValue) body : new MappingJacksonValue(body));
+    }
+}
---
+++ src/main/java/com/cex0/mobiai/core/ControllerExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.cex0.mobiai.core;
+
+import com.cex0.mobiai.model.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @Auther: wodenvyoujiaoshaxiong
+ * @Date: 2019/12/26 01:10
+ * @Description:
+ */
+@RestControllerAdvice
+@Slf4j
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public BaseResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        BaseResponse<?> baseResponse = handleBaseException(e);
+        baseResponse.setMessage(String.format("请求字段缺失, 类型为 %s，名称为 %s", e.getParameterType(), e.getParameterName()));
+        return baseResponse;
+    }
+
+    private <T> BaseResponse<T> handleBaseException(Throwable t) {
+        Assert.notNull(t, "Throwable must not be null");
+
+        log.error("Captured an exception", t);
+
+        BaseResponse<T> baseResponse = new BaseResponse<>();
+        baseResponse.setMessage(t.getMessage());
+
+        if (log.isDebugEnabled()) {
+            baseResponse.setDevMessage(ExceptionUtils.getStackTrace(t));
+        }
+
+        return baseResponse;
+    }
+}
